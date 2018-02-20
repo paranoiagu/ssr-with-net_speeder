e0b2033ae02fa90682aaa0b2efb736ab36e24a93
Paranoia Gu <gu.yanfeng@gmail.com>
2018-02-20 14:06:20 +0800

    write ss server args

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,4 @@ echo "========================================================================"
 
 nohup /usr/sbin/sshd -D &
 nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
-python /root/ssr/shadowsocks/server.py "$@"
+python /root/ssr/shadowsocks/server.py -s 0.0.0.0 -p 8388 -k pass -m chacha20 -o http_post_compatible -O auth_sha1_v4_compatible
